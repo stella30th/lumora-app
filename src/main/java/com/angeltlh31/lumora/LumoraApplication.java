842ecc49f81a53be94d51d5c2117e842787f3bea
holethienan30102006@gmail.com <holethienan30102006@gmail.com>
2026-08-12 20:21:36 +0700

    1

---
+++ src/main/java/com/angeltlh31/lumora/LumoraApplication.java
@@ -0,0 +1,13 @@
+package com.angeltlh31.lumora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LumoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/angeltlh31/lumora/LumoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angeltlh31.lumora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
